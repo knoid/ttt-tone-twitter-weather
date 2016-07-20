d4dbcb750c4c6a09801d97df5a840c85e1898ae5
Ariel Barabas <abarabas@ar.ibm.com>
2016-07-20 15:42:56 -0300

    using bundled  method. fixed directory path

--- server/app.js
+++ server/app.js
@@ -2,13 +2,12 @@ var
 
 express = require('express'),
 morgan = require('morgan'),
-serveStatic = require('serve-static'),
 
 port = process.env.PORT || 3000;
 
 express().
   use(morgan('dev')).
-  use(serveStatic(__dirname + '/public')).
+  use(express.static(__dirname + '/../public')).
   get('/output', require('../EDIT_ME')).
   listen(port, function() {
     console.log('Server started, listening on port', port);
